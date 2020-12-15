2ef7b8e3d5212c0ef6c8fc15120b1ed47dde71ff
fgulpolat <49431870+fgulpolat@users.noreply.github.com>
2020-12-15 17:12:19 +0300

    Core layer comleted

---
+++ ToDoListApp.Core/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListApp.Core.Entities;
+
+namespace ToDoListApp.Core.Data.Abstract
+{
+   public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T Get(Expression<Func<T, bool>> predicate);
+        T GetById(int id);
+        T Add(T entity);
+        void Delete(T entity);
+        T Update(T entity);
+    
+    }
+}
---
+++ ToDoListApp.Core/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListApp.Core.Data.Abstract;
+using ToDoListApp.Core.Entities;
+
+namespace ToDoListApp.Core.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext> : IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+        where TContext:DbContext,new()
+    {
+
+        public TEntity Add(TEntity entity)
+        {
+           using(var context=new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+           using(var context=new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(predicate);
+            }
+        }
+
+        public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            using (var context = new TContext())
+            {
+                return predicate == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(predicate).ToList();
+            }
+        }
+
+        public TEntity GetById(int id)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+    }
+}
